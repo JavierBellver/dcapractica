0a48c01d09948806f97ecb91d9a3f4bec24b9f03
Javier Bellver García <jbellver271@gmail.com>
2016-12-21 17:42:08 +0100

    First commit of the saludador project

---
+++ src/saludadormain.cc
@@ -0,0 +1,26 @@
+#include <libintl.h>
+#include<stdio.h>
+#include <iostream>
+using namespace std;
+
+#define _(x) gettext(x)
+int main() {
+	string DEVELOPMENT_MODE   ="ON";
+	string GETTEXT_PACKAGE    = "saludador";
+	string PACKAGE_LOCALE_DIR ="/usr/local/share/locale/";
+	string PROJECT_DIR ="/home/jb/DCA/Practica8";
+
+	string localedir;
+
+	if (DEVELOPMENT_MODE == "ON") { // App not installed,
+                localedir = "./po";
+        } else {
+                localedir = PACKAGE_LOCALE_DIR;
+        }
+	setlocale (LC_MESSAGES, "");
+	bindtextdomain (GETTEXT_PACKAGE.c_str(), localedir.c_str());
+	bind_textdomain_codeset (GETTEXT_PACKAGE.c_str(), "UTF-8");
+	textdomain (GETTEXT_PACKAGE.c_str());
+	cout << (_("saludo\n"));
+	return 0;
+}
